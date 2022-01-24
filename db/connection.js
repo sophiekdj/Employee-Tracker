31c0984907bafeca47203d82c60dd8ea839b94db
sophiekdj <s.dejong057@gmail.com>
2022-01-24 23:30:39 +0800

    changes to connection file

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,14 @@
+// Import and require mysql2
 const mysql = require("mysql2");
 
+// Connect to database
 const connection = mysql.createConnection({
   host: "localhost",
   // Your username
   user: "root",
   // Your password
   password: "",
-  database: "employees"
+  database: "employees",
 });
 
 connection.connect(function (err) {
